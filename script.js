d4db2af691b8aa2a829bae7e4c9854cfe75b8bfe
Gustavo Baron <gustavoalecxander9@gmail.com>
2026-04-28 21:28:00 -0300

    Refactor task input event listener and comments
    atualização

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ document.addEventListener('DOMContentLoaded', () => {
     addTaskBtn.addEventListener('click', addTask);
 
     // Permite adicionar a tarefa pressionando a tecla "Enter"
-    /*taskInput.addEventListener('keypress', function(e) {
+    taskInput.addEventListener('keypress', function(e) {
         if (e.key === 'Enter') {
             addTask();
         }
-    });*/
+    });
 
     function addTask() {
         const taskText = taskInput.value;
 
-        // RN01 - Prevenção de Tarefa Vazia: Valida se o input está em branco ou só tem espaços
+        // Valida se o input está em branco ou só tem espaços
         if (taskText.trim() === '') {
             alert('A tarefa não pode estar vazia. Por favor, insira um texto.');
             return;
         }
 
-        // Criação do item da lista (RF02)
+        // Criação do item da lista 
         const li = document.createElement('li');
 
-        // Checkbox para controle de status (RF03)
+        // Checkbox para controle de status 
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
         
-        // Evento para marcar/desmarcar a tarefa (RN02)
+        // marcar/desmarcar a tarefa
         checkbox.addEventListener('change', function() {
             if (this.checked) {
                 li.classList.add('completed');
@@ -38,21 +38,17 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
 
-        // Elemento de texto da tarefa
-        // RN04 - Imutabilidade: O texto é inserido em um span estático e sem botão de exclusão
+        // O texto é inserido em um span estático e sem botão de exclusão
         const span = document.createElement('span');
         span.className = 'task-text';
         span.textContent = taskText;
 
-        // Monta o elemento na tela (RNF01 - Usabilidade e dinamismo sem recarregar)
-        li.appendChild(checkbox);
+        // Monta o elemento na tela 
         li.appendChild(span);
         taskList.appendChild(li);
 
         // Limpa o campo de texto para a próxima inserção
         taskInput.value = '';
         taskInput.focus();
-        
-        // RNF03 - Volatilidade: Os dados estão apenas no DOM e sumirão ao recarregar a página.
     }
 });
